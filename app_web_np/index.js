93df1a57c5852fea8445e68f1dbed945182ea91a
oPatao <ghoppvp2407@gmail.com>
2024-04-04 11:07:07 -0300

    app web 0.0.1

--- app_web_np/index.js
+++ app_web_np/index.js
@@ -1,1 +1,14 @@
- console.log('hello world');
+const express = require('express');
+const app = express();
+
+const PORT = 8080;
+
+app.get('/', function(req,res){
+    res.send('salve mundão bonito')
+})
+
+app.listen(PORT, function(){
+    console.log('ta funfando');
+});
+ 
+
